73c0a3d2ec4458c2c75d98d4f3d629dc72bc0747
leimaau <229042136@qq.com>
2020-02-24 00:16:47 +0800

    fix

--- js/config.js
+++ js/config.js
@@ -38,11 +38,11 @@ bootstrap-select 本地化配置
 const colData = [
 	{ field: 'YEAR', title: '資料', align: 'center', formatter: (value) => { return `${value.replace('_bw', '')}`; } }
 	//, {field: 'ID',title: 'ID'}
-	, { field: 'TRAD', title: '繁體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char')">${value}</a>`; } }
-	, { field: 'SIMP', title: '簡體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char_simp')">${value}</a>`; } }
+	, { field: 'TRAD', title: '繁體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char', 'dicWord')">${value}</a>`; } }
+	, { field: 'SIMP', title: '簡體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char_simp', 'dicWord')">${value}</a>`; } }
 	, { field: 'IPA_S', title: '原文IPA' }
 	, { field: 'IPA_T', title: '統一IPA' }
-	, { field: 'JYUTPING', title: '粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value.slice(0, -1)}', 'jyutping')">${value.slice(0, -1)}</a>${value.slice(-1)}`; } }
+	, { field: 'JYUTPING', title: '粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value.slice(0, -1)}', 'jyutping', 'dicWord')">${value.slice(0, -1)}</a>${value.slice(-1)}`; } }
 	, { field: 'SOUR', title: '來源', formatter: (value, row) => { return formatSOUR(value, row['YEAR'].replace('_bw', ''), 'png') } }
 	, { field: 'EXPL', title: '詞例', formatter: (value) => { return `<p data-toggle="tooltip"  title='${value}'>${value}<p/>` } }
 	, { field: 'NOTE', title: 'leimaau附註', formatter: (value) => { return `<p data-toggle="tooltip" title='${value}'>${value}<p/>` } }
@@ -51,7 +51,7 @@ const colData = [
 const colData_gw = [
 	{ field: 'YEAR', title: '資料', align: 'center' }
 	//, {field: 'ID',title: 'ID'}
-	, { field: 'WORD', title: '字', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char')">${value}</a>`; } }
+	, { field: 'WORD', title: '字', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'char', 'dicWord')">${value}</a>`; } }
 	, { field: 'EXPL', title: '字義', formatter: (value) => { return `<p data-toggle="tooltip" title='${value}'>${value}<p/>` }  }
 	, { field: 'FINAL_PART', title: '韻部' }
 	, { field: 'FIRST_OLD', title: '聲母' }
@@ -62,7 +62,7 @@ const colData_gw = [
 	, { field: 'VOLUME', title: '冊' }
 	, { field: 'PAGE', title: '葉', formatter: (value, row) => { return pageSplit(value, 'jpg', row['VOLUME']=='上冊' ? 'http://ytenx.org/static/img/KrungGhoTchiekDukPyonYonhTsuatQjeuhGhopDzip/volume1/' : 'http://ytenx.org/static/img/KrungGhoTchiekDukPyonYonhTsuatQjeuhGhopDzip/volume2/') } }
 	, { field: 'IPA', title: '擬音IPA' }
-	, { field: 'JYUTPING', title: '擬音粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value.slice(0, -1)}', 'jyutping')">${value.slice(0, -1)}</a>${value.slice(-1)}`; } }
+	, { field: 'JYUTPING', title: '擬音粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value.slice(0, -1)}', 'jyutping', 'dicWord')">${value.slice(0, -1)}</a>${value.slice(-1)}`; } }
 
 ];
 
@@ -72,11 +72,11 @@ const colData_phrase = [
 	, { field: 'LEVEL1', title: '一級分類', align: 'center' }
 	, { field: 'LEVEL2', title: '二級分類' }
 	, { field: 'LEVEL3', title: '三級分類', align: 'center' }
-	, { field: 'TRAD', title: '繁體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase')">${value}</a>`; } }
-	, { field: 'SIMP', title: '簡體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase_simp')">${value}</a>`; } }
+	, { field: 'TRAD', title: '繁體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase', 'dicPhrase')">${value}</a>`; } }
+	, { field: 'SIMP', title: '簡體', align: 'center', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase_simp', 'dicPhrase')">${value}</a>`; } }
 	, { field: 'IPA_S', title: '原文IPA' }
 	, { field: 'IPA_T', title: '統一IPA' }
-	, { field: 'JYUTPING', title: '粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase_jyut6ping3')">${value}</a>`; } }
+	, { field: 'JYUTPING', title: '粵拼', formatter: (value) => { return `<a href="javascript:querySubmit('${value}', 'phrase_jyut6ping3', 'dicPhrase')">${value}</a>`; } }
 	, { field: 'SOUR', title: '來源', formatter: (value, row) => { return formatSOUR(value, row['YEAR'].replace('_phrase', ''), 'png') } }
 	, { field: 'EXPL', title: '解釋', formatter: (value) => { return `<p data-toggle="tooltip"  title='${value}'>${value}<p/>` } }
 	, { field: 'NOTE', title: 'leimaau附註', formatter: (value) => { return `<p data-toggle="tooltip" title='${value}'>${value}<p/>` } }
